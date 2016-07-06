280658ed11f76b1113fe1653e969504e10e0dca8
Nathan Bunker <Nathan.Bunker@gmail.com>
2016-07-06 11:25:00 -0600

    Added Antigen and CVX servlet

---
+++ src/main/java/org/openimmunizationsoftware/cdsi/servlet/DataModelViewCvxServlet.java
@@ -0,0 +1,41 @@
+package org.openimmunizationsoftware.cdsi.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.openimmunizationsoftware.cdsi.core.data.DataModel;
+
+public class DataModelViewCvxServlet extends ForecastServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    HttpSession session = req.getSession(true);
+    DataModel dataModel = (DataModel) session.getAttribute("dataModel");
+    if (dataModel == null) {
+      return;
+    }
+
+    resp.setContentType("text/html");
+    PrintWriter out = new PrintWriter(resp.getOutputStream());
+    try {
+      out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\">");
+      out.println("<html>");
+      out.println("  <head>");
+      out.println("    <title>CDSi - Data Model View - CVX</title>");
+      out.println("    <link rel=\"stylesheet\" type=\"text/css\" href=\"index.css\">");
+      out.println("  </head>");
+      out.println("  <body>");
+      // Do stuff here .....
+      out.println("  </body>");
+      out.println("</html>");
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      out.close();
+    }
+  }
+}
