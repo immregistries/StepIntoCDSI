d4194cfb4889013bf16263942f2847dd62e0bb84
Lairon764 <leehgrygl@gmail.com>
2016-07-08 15:59:56 -0600

    Vaccine group servlet

--- src/main/java/org/openimmunizationsoftware/cdsi/servlet/dataModelView/VaccineGroupServlet.java
+++ src/main/java/org/openimmunizationsoftware/cdsi/servlet/dataModelView/VaccineGroupServlet.java
@@ -1,6 +1,8 @@
 package org.openimmunizationsoftware.cdsi.servlet.dataModelView;
 
 import org.openimmunizationsoftware.cdsi.core.data.DataModel;
+import org.openimmunizationsoftware.cdsi.core.domain.VaccineGroup;
+import org.openimmunizationsoftware.cdsi.core.domain.VaccineType;
 import org.openimmunizationsoftware.cdsi.servlet.ForecastServlet;
 
 import javax.servlet.ServletException;
@@ -14,6 +16,13 @@ import java.io.PrintWriter;
  * Created by Eric on 7/7/16.
  */
 public class VaccineGroupServlet extends MainServlet {
+  public static final String SERVLET_NAME = "dataModelViewVaccineGroup";
+  private static final String PARAM_SEARCH = "search_term";
+
+  public static String makeLink(VaccineGroup vaccineGroup) {
+    return "<a href=\"" + SERVLET_NAME + "?" + PARAM_SEARCH + "=" + vaccineGroup.getName() + "\" target=\"dataModelView\">" + vaccineGroup.getName() + "</a>";
+  }
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     HttpSession session = req.getSession(true);
@@ -23,10 +32,56 @@ public class VaccineGroupServlet extends MainServlet {
     }
     resp.setContentType("text/html");
 
+    String search_term = req.getParameter(PARAM_SEARCH);
+    if (search_term == null) {
+      search_term = "";
+    }
     PrintWriter out = new PrintWriter(resp.getOutputStream());
     try {
       printHeader(out, "Vaccine Group");
-      // TODO: Lee, do something here
+
+      out.println("    <form action=\"" + SERVLET_NAME + "\">");
+
+      out.println("      <input type=\"text\" name=\""+ PARAM_SEARCH + "\"><br>");
+      out.println("      <input type=\"submit\" value=\"Submit\">");
+
+      out.println("    </form>");
+
+      out.println("  <table>");
+      out.println("   <tr>");
+      out.println("      <th>Name</td>");
+      out.println("      <th>Vaccine List</td>");
+      out.println("      <th>Vaccine Group Forecast</td>");
+      out.println("      <th>Administer Full Vaccine Group</td>");
+      out.println("      <th>Antigen List</td>");
+      out.println("   </tr>");
+
+      if (search_term.length() >= 1) {
+        VaccineGroup vaccineGroup = dataModel.getVaccineGroupMap().get(search_term);
+        out.println("   <tr>");
+        out.println("      <td>" + makeLink(vaccineGroup) + "</td>");
+        out.println("      <td>" + vaccineGroup.getVaccineList() + "</td>");
+        out.println("      <td>" + vaccineGroup.getVaccineGroupForecast() + "</td>");
+        out.println("      <td>" + vaccineGroup.getAdministerFullVaccineGroup() + "</td>");
+        out.println("      <td>" + vaccineGroup.getAntigenList() + "</td>");
+
+        out.println("   </tr>");
+      } else {
+        for(VaccineGroup vaccineGroup: dataModel.getVaccineGroupMap().values()){
+          // I am most likely doing the wrong thing here. :)
+          out.println("   <tr>");
+          out.println("      <td>" + makeLink(vaccineGroup) + "</td>");
+          out.println("      <td>" + vaccineGroup.getVaccineList() + "</td>");
+          out.println("      <td>" + vaccineGroup.getVaccineGroupForecast() + "</td>");
+          out.println("      <td>" + vaccineGroup.getAdministerFullVaccineGroup() + "</td>");
+          out.println("      <td>" + vaccineGroup.getAntigenList() + "</td>");
+          out.println("   </tr>");
+        }
+      }
+      // Do stuff here .....
+      out.println("  </table>");
+
+
       printFooter(out);
     } catch (Exception e) {
       e.printStackTrace();
