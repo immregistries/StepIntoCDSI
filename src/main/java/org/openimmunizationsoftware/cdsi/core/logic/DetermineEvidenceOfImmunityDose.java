66b6e116c69e65488d88b803e21ef22192d060d6
Nathan Bunker <Nathan.Bunker@gmail.com>
2017-01-05 10:11:24 +0100

    Fixing logic table setups

--- src/main/java/org/openimmunizationsoftware/cdsi/core/logic/DetermineEvidenceOfImmunityDose.java
+++ src/main/java/org/openimmunizationsoftware/cdsi/core/logic/DetermineEvidenceOfImmunityDose.java
@@ -20,8 +20,7 @@ import static org.openimmunizationsoftware.cdsi.core.logic.items.LogicResult.ANY
 import static org.openimmunizationsoftware.cdsi.core.logic.items.LogicResult.NO;
 import static org.openimmunizationsoftware.cdsi.core.logic.items.LogicResult.YES;
 
-public class DetermineEvidenceOfImmunityDose extends LogicStep
-{
+public class DetermineEvidenceOfImmunityDose extends LogicStep {
 
   private ConditionAttribute<Date> caDateofBirth = null;
   private ConditionAttribute<String> caCountryofBirth = null;
@@ -38,10 +37,13 @@ public class DetermineEvidenceOfImmunityDose extends LogicStep
     caDateofBirth = new ConditionAttribute<Date>("Patient Data", "Date of Birth");
     caCountryofBirth = new ConditionAttribute<String>("Patient Data", "Country of Birth");
     caMaximumAgeDate = new ConditionAttribute<Date>("Calculated date (CALCDTAGE-1)", "Maximum Age Date");
-    caImmunityGuideline = new ConditionAttribute<String>("Supporting Data (Clinical History Immunity","Immunity Guideline");
+    caImmunityGuideline = new ConditionAttribute<String>("Supporting Data (Clinical History Immunity",
+        "Immunity Guideline");
     caImmunityDate = new ConditionAttribute<Date>("Supporting Data (Birth Date Immunity", "Immunity Date");
-    caExclusionCondition = new ConditionAttribute<String>("Supporting Data (Birth Date Immunity)", "Exclusion Condition");
-    caCountryofBirthWorking = new ConditionAttribute<String>("Supporting Data (Birth Date Immunity)", "Country of Birth");
+    caExclusionCondition = new ConditionAttribute<String>("Supporting Data (Birth Date Immunity)",
+        "Exclusion Condition");
+    caCountryofBirthWorking = new ConditionAttribute<String>("Supporting Data (Birth Date Immunity)",
+        "Country of Birth");
 
     caMaximumAgeDate.setAssumedValue(FUTURE);
 
@@ -55,15 +57,11 @@ public class DetermineEvidenceOfImmunityDose extends LogicStep
 
     LT logicTable = new LT();
     logicTableList.add(logicTable);
-    
+
     /**
      * Need to instansiate the attributes
      */
-    
-    
-    
-    
-    
+
   }
 
   @Override
@@ -84,146 +82,145 @@ public class DetermineEvidenceOfImmunityDose extends LogicStep
 
   private void printStandard(PrintWriter out) {
     out.println("<h1> " + logicStepType.getDisplay() + "</h1>");
-    out.println("<p>Determine evidence of immunity  assesses the patient’s profile to determine if the patient is already potentially immune to the target disease, negating the need for additional doses.</p>");
-    
+    out.println(
+        "<p>Determine evidence of immunity  assesses the patient’s profile to determine if the patient is already potentially immune to the target disease, negating the need for additional doses.</p>");
+
     System.out.println(caImmunityGuideline);
-    
-    
-    
+
     printConditionAttributesTable(out);
     printLogicTables(out);
   }
 
-  private class LT extends LogicTable
-  {
-   public LT() {
-     super(4, 5, "Table 5-3 Does the patient have evidence of immunity ?");
-     
-     setLogicCondition(0, new LogicCondition("Does the patient clinical history contain one of the supporting data defined immunity guidelines ?") {
-    	 @Override
-    	 protected LogicResult evaluateInternal() {
-			List<Immunity> immunityList = dataModel.getImmunityList();
-			int immunityListLength = immunityList.size();
-			for(int j=0; j<immunityListLength;j++){
-				List<ClinicalHistory> clinicalHistoryList = dataModel.getImmunityList().get(j).getClinicalHistoryList();
-				int clinicalHistoryLength = clinicalHistoryList.size();
-				for(int i=0; i<clinicalHistoryLength;i++){
-					if(clinicalHistoryList.get(i).getImmunityGuideline().equals(caImmunityGuideline)){
-						return LogicResult.YES;
-					}
-				}
-			}
-			 
-			return LogicResult.NO;
-		}
-	});
-     
-     setLogicCondition(1, new LogicCondition("Is the patient's date of the birth < the supporting data defined immunity date ?") {
-    	 @Override
-    	 protected LogicResult evaluateInternal() {
-			if (caDateofBirth.getAssumedValue().before(caImmunityDate.getAssumedValue())) {
-				return LogicResult.YES;			
-			}else{
-				return LogicResult.NO;
-			}
-		}
-	});
-     
-     setLogicCondition(2, new LogicCondition("Does the patient have the an exclusion condition to  the immunity ?") {
-    	 @Override
-    	 protected LogicResult evaluateInternal() {
-			List<Immunity> immunityList = dataModel.getImmunityList();
-			int immunityListLength = immunityList.size();
-			for(int j=0; j<immunityListLength;j++){
-				List<BirthDateImmunity> birthDateImmunityList = dataModel.getImmunityList().get(j).getBirthDateImmunityList();
-				int birthDateImmunityListLength = birthDateImmunityList.size();
-				for(int i=0; i<birthDateImmunityListLength;i++){
-					List<Exclusion> exclusionList = birthDateImmunityList.get(i).getExclusionList();
-					int exclusionListLength = exclusionList.size();
-					for(int k=0; k<exclusionListLength;k++){
-						Exclusion exclusion = exclusionList.get(k);
-						if(exclusion.getExclusionCondition().equals(caExclusionCondition)){
-							return LogicResult.YES;
-						}
-					}	
-				}
-			}
-			 
-			return LogicResult.NO;
-		}
-	});
-     
-    setLogicCondition(3, new LogicCondition("Is the patient's country of birth the same as the supporting data defined country of birth ?") {
-		@Override
-   	 protected LogicResult evaluateInternal() {
-			List<Immunity> immunityList = dataModel.getImmunityList();
-			int immunityListLength = immunityList.size();
-			for(int j=0; j<immunityListLength;j++){
-				List<BirthDateImmunity> birthDateImmunityList = dataModel.getImmunityList().get(j).getBirthDateImmunityList();
-				int birthDateImmunityListLength = birthDateImmunityList.size();
-				for(int i=0; i<birthDateImmunityListLength;i++){
-					String countryOfBirth = birthDateImmunityList.get(i).getCountryOfBirth();
-					if(countryOfBirth.equals(caCountryofBirth)){
-						return LogicResult.YES;
-								
-					}
-	
-				}
-			}
-			 
-			return LogicResult.NO;
-		}
-	});
-    
-    setLogicResults(0,YES,ANY,ANY,ANY,ANY);
-    setLogicResults(1,NO,YES,YES,ANY);
-    setLogicResults(2,NO,YES,NO,YES);
-    setLogicResults(3,NO,YES,NO,YES);
-    setLogicResults(4,NO,NO,ANY,ANY);
-    
-    
-    setLogicOutcome(0, new LogicOutcome() {
-		@Override
-		public void perform() {
-			log("Yes. The patient has evidence of immunity.");
-			dataModel.getPatientSeries().setPatientSeriesStatus(PatientSeriesStatus.IMMUNE);
-			log("Forecast reason is \"patient has evidence of immunity\". ");
-		}
-	});
-    
-    setLogicOutcome(1, new LogicOutcome() {
-		@Override
-		public void perform() {
-			log("No. The patient does not have evidence of immunity.");
-		}
-	});
-    
-    setLogicOutcome(2, new LogicOutcome() {
-  		@Override
-  		public void perform() {
-  			log("Yes. The patient has evidence of immunity.");
-  			dataModel.getPatientSeries().setPatientSeriesStatus(PatientSeriesStatus.IMMUNE);
-  			log("Forecast reason is \"patient has evidence of immunity\". ");
-  		}
-  	});
-      
+  private class LT extends LogicTable {
+    public LT() {
+      super(4, 5, "Table 5-3 Does the patient have evidence of immunity ?");
+
+      setLogicCondition(0, new LogicCondition(
+          "Does the patient clinical history contain one of the supporting data defined immunity guidelines ?") {
+        @Override
+        protected LogicResult evaluateInternal() {
+          List<Immunity> immunityList = dataModel.getImmunityList();
+          int immunityListLength = immunityList.size();
+          for (int j = 0; j < immunityListLength; j++) {
+            List<ClinicalHistory> clinicalHistoryList = dataModel.getImmunityList().get(j).getClinicalHistoryList();
+            int clinicalHistoryLength = clinicalHistoryList.size();
+            for (int i = 0; i < clinicalHistoryLength; i++) {
+              if (clinicalHistoryList.get(i).getImmunityGuideline().equals(caImmunityGuideline)) {
+                return LogicResult.YES;
+              }
+            }
+          }
+
+          return LogicResult.NO;
+        }
+      });
+
+      setLogicCondition(1,
+          new LogicCondition("Is the patient's date of the birth < the supporting data defined immunity date ?") {
+            @Override
+            protected LogicResult evaluateInternal() {
+              if (caDateofBirth.getAssumedValue().before(caImmunityDate.getAssumedValue())) {
+                return LogicResult.YES;
+              } else {
+                return LogicResult.NO;
+              }
+            }
+          });
+
+      setLogicCondition(2, new LogicCondition("Does the patient have the an exclusion condition to  the immunity ?") {
+        @Override
+        protected LogicResult evaluateInternal() {
+          List<Immunity> immunityList = dataModel.getImmunityList();
+          int immunityListLength = immunityList.size();
+          for (int j = 0; j < immunityListLength; j++) {
+            List<BirthDateImmunity> birthDateImmunityList = dataModel.getImmunityList().get(j)
+                .getBirthDateImmunityList();
+            int birthDateImmunityListLength = birthDateImmunityList.size();
+            for (int i = 0; i < birthDateImmunityListLength; i++) {
+              List<Exclusion> exclusionList = birthDateImmunityList.get(i).getExclusionList();
+              int exclusionListLength = exclusionList.size();
+              for (int k = 0; k < exclusionListLength; k++) {
+                Exclusion exclusion = exclusionList.get(k);
+                if (exclusion.getExclusionCondition().equals(caExclusionCondition)) {
+                  return LogicResult.YES;
+                }
+              }
+            }
+          }
+
+          return LogicResult.NO;
+        }
+      });
+
+      setLogicCondition(3, new LogicCondition(
+          "Is the patient's country of birth the same as the supporting data defined country of birth ?") {
+        @Override
+        protected LogicResult evaluateInternal() {
+          List<Immunity> immunityList = dataModel.getImmunityList();
+          int immunityListLength = immunityList.size();
+          for (int j = 0; j < immunityListLength; j++) {
+            List<BirthDateImmunity> birthDateImmunityList = dataModel.getImmunityList().get(j)
+                .getBirthDateImmunityList();
+            int birthDateImmunityListLength = birthDateImmunityList.size();
+            for (int i = 0; i < birthDateImmunityListLength; i++) {
+              String countryOfBirth = birthDateImmunityList.get(i).getCountryOfBirth();
+              if (countryOfBirth.equals(caCountryofBirth)) {
+                return LogicResult.YES;
+
+              }
+
+            }
+          }
+
+          return LogicResult.NO;
+        }
+      });
+
+      setLogicResults(0, YES, NO, NO, NO, NO);
+      setLogicResults(1, ANY, YES, YES, YES, NO);
+      setLogicResults(2, ANY, NO, NO, NO, ANY);
+      setLogicResults(3, ANY, ANY, YES, NO, ANY);
+
+      setLogicOutcome(0, new LogicOutcome() {
+        @Override
+        public void perform() {
+          log("Yes. The patient has evidence of immunity.");
+          dataModel.getPatientSeries().setPatientSeriesStatus(PatientSeriesStatus.IMMUNE);
+          log("Forecast reason is \"patient has evidence of immunity\". ");
+        }
+      });
+
+      setLogicOutcome(1, new LogicOutcome() {
+        @Override
+        public void perform() {
+          log("No. The patient does not have evidence of immunity.");
+        }
+      });
+
+      setLogicOutcome(2, new LogicOutcome() {
+        @Override
+        public void perform() {
+          log("Yes. The patient has evidence of immunity.");
+          dataModel.getPatientSeries().setPatientSeriesStatus(PatientSeriesStatus.IMMUNE);
+          log("Forecast reason is \"patient has evidence of immunity\". ");
+        }
+      });
+
       setLogicOutcome(3, new LogicOutcome() {
-  		@Override
-  		public void perform() {
-  			log("No. The patient does not have evidence of immunity.");
-  		}
-  	});
-      
+        @Override
+        public void perform() {
+          log("No. The patient does not have evidence of immunity.");
+        }
+      });
+
       setLogicOutcome(4, new LogicOutcome() {
-  		@Override
-  		public void perform() {
-  			log("No. The patient does not have evidence of immunity.");
-  		}
-  	});
+        @Override
+        public void perform() {
+          log("No. The patient does not have evidence of immunity.");
+        }
+      });
 
     }
   }
-  
-  
 
 }
--- src/main/java/org/openimmunizationsoftware/cdsi/core/logic/EvaluateLiveVirusConflict.java
+++ src/main/java/org/openimmunizationsoftware/cdsi/core/logic/EvaluateLiveVirusConflict.java
@@ -16,34 +16,34 @@ import org.openimmunizationsoftware.cdsi.core.logic.items.LogicOutcome;
 import org.openimmunizationsoftware.cdsi.core.logic.items.LogicResult;
 import org.openimmunizationsoftware.cdsi.core.logic.items.LogicTable;
 
-public class EvaluateLiveVirusConflict extends LogicStep
-{
+public class EvaluateLiveVirusConflict extends LogicStep {
 
-   private ConditionAttribute<Date> caDateAdministered = null;
-   private ConditionAttribute<Date> caConflictBeginIntervalDate = null;
-   private ConditionAttribute<Date> caConflictEndIntervalDate = null;
-   private ConditionAttribute<VaccineType> caCurrentVaccineType = null;
-   private ConditionAttribute<VaccineType> caPreviousVaccineType = null;
-   
+  private ConditionAttribute<Date> caDateAdministered = null;
+  private ConditionAttribute<Date> caConflictBeginIntervalDate = null;
+  private ConditionAttribute<Date> caConflictEndIntervalDate = null;
+  private ConditionAttribute<VaccineType> caCurrentVaccineType = null;
+  private ConditionAttribute<VaccineType> caPreviousVaccineType = null;
 
-  public EvaluateLiveVirusConflict(DataModel dataModel)
-  {
+  public EvaluateLiveVirusConflict(DataModel dataModel) {
     super(LogicStepType.EVALUATE_FOR_LIVE_VIRUS_CONFLICT, dataModel);
     setConditionTableName("Table ");
-    
+
     caDateAdministered = new ConditionAttribute<Date>("Vaccine dose administered", "Date Administered");
-    caConflictBeginIntervalDate = new ConditionAttribute<Date>("Calculated date (CALCDTLIVE-1)" , "Conflict Begin Interval Date");
-    caConflictEndIntervalDate = new ConditionAttribute<Date>("Calculated date(CALCDTLIVE-2 & CALCDTLIVE-3" , "Conflict End Interval Date");
-    caCurrentVaccineType = new ConditionAttribute<VaccineType>("Supporting Data (Live Virus Conflict)" , "Current Vaccine Type");
-    caPreviousVaccineType = new ConditionAttribute<VaccineType>("Supporting Data (Live Virus Conflict)" ,  "Previous Vaccine Type");
-    
+    caConflictBeginIntervalDate = new ConditionAttribute<Date>("Calculated date (CALCDTLIVE-1)",
+        "Conflict Begin Interval Date");
+    caConflictEndIntervalDate = new ConditionAttribute<Date>("Calculated date(CALCDTLIVE-2 & CALCDTLIVE-3",
+        "Conflict End Interval Date");
+    caCurrentVaccineType = new ConditionAttribute<VaccineType>("Supporting Data (Live Virus Conflict)",
+        "Current Vaccine Type");
+    caPreviousVaccineType = new ConditionAttribute<VaccineType>("Supporting Data (Live Virus Conflict)",
+        "Previous Vaccine Type");
+
     conditionAttributesList.add(caDateAdministered);
     conditionAttributesList.add(caConflictBeginIntervalDate);
     conditionAttributesList.add(caConflictEndIntervalDate);
     conditionAttributesList.add(caCurrentVaccineType);
     conditionAttributesList.add(caPreviousVaccineType);
-    
-    
+
     LT420 logicTable = new LT420();
     logicTableList.add(logicTable);
   }
@@ -66,165 +66,162 @@ public class EvaluateLiveVirusConflict extends LogicStep
 
   private void printStandard(PrintWriter out) {
     out.println("<h1> " + getTitle() + "</h1>");
-    
-    out.println("<p>Evaluate  live virus conflict  validates the  date  administered  of a live virus  vaccine dose administered  against previous  live  virus  administered  vaccines  to  ensure  proper  spacing  between  administrations.  For  some  live virus vaccines and for inactivated  virus or recombinant  vaccines, this condition does not exist. Therefore, if no live  virus  supporting  data  exists  for  the  vaccine  dose  administered  being  evaluated,  the  vaccine  dose administered is not in conflict with any other vaccine dose administered.</p>");//<------------------------------------------------------
+
+    out.println(
+        "<p>Evaluate  live virus conflict  validates the  date  administered  of a live virus  vaccine dose administered  against previous  live  virus  administered  vaccines  to  ensure  proper  spacing  between  administrations.  For  some  live virus vaccines and for inactivated  virus or recombinant  vaccines, this condition does not exist. Therefore, if no live  virus  supporting  data  exists  for  the  vaccine  dose  administered  being  evaluated,  the  vaccine  dose administered is not in conflict with any other vaccine dose administered.</p>");// <------------------------------------------------------
 
     printConditionAttributesTable(out);
     printLogicTables(out);
   }
 
-  private class LT420 extends LogicTable
-  {
+  private class LT420 extends LogicTable {
     public LT420() {
-    	
-     
-    	super(2, 3, "Table 4-20 Should the current vaccine dose administrated be evaluted for a live virus conflict ? ");
 
-    	
-    	setLogicCondition(0, new LogicCondition("Is the current vaccine type of the vaccine dose administered one of the supporting data defined live virus conflict current vaccine types?"){
-			@Override
-			protected LogicResult evaluateInternal() {
-				for(LiveVirusConflict liveVirusConflict: dataModel.getLiveVirusConflictList()){
-					if(liveVirusConflict.getCurrentVaccineType().equals(caCurrentVaccineType)){
-						return YES;
-					}	
-				}
-				return NO;
-			}	
-    	});
-    	
-    	setLogicCondition(1, new LogicCondition("Is there at least one vaccine dose administered on or before the current vaccine dose administered date?") {//CHeck
-			
-			@Override
-			protected LogicResult evaluateInternal() {
-				for(AntigenAdministeredRecord aar: dataModel.getAntigenAdministeredRecordList()){
-					
-					if(!aar.getDateAdministered().after(caDateAdministered.getFinalValue())){
-						return YES;
-					}
-				}
-				return NO;
-			}
-		});
-    	
-    	setLogicResults(0, YES,YES);
-    	setLogicResults(1, NO,ANY);
-    	setLogicResults(2, ANY,NO);
-    	
-    	setLogicOutcome(0, new LogicOutcome() {
-			@Override
-			public void perform() {
-				log("Yes. The vaccine dose administered should be evaluated for a live virus conflict");
-			}
-		});
-    	
-    	setLogicOutcome(1, new LogicOutcome() {
-			@Override
-			public void perform() {	
-				log("No. The vaccine dose administered should not be evaluated for a live virus conflict.");
-			}
-		});
-    	
-    	setLogicOutcome(2, new LogicOutcome() {
-			
-			@Override
-			public void perform() {
-				log("No. The vaccine dose administered should not be evaluated for a live virus conflict.");
-			}
-		});  	
-     
+      super(2, 3, "Table 4-20 Should the current vaccine dose administrated be evaluted for a live virus conflict ? ");
+
+      setLogicCondition(0, new LogicCondition(
+          "Is the current vaccine type of the vaccine dose administered one of the supporting data defined live virus conflict current vaccine types?") {
+        @Override
+        protected LogicResult evaluateInternal() {
+          for (LiveVirusConflict liveVirusConflict : dataModel.getLiveVirusConflictList()) {
+            if (liveVirusConflict.getCurrentVaccineType().equals(caCurrentVaccineType)) {
+              return YES;
+            }
+          }
+          return NO;
+        }
+      });
+
+      setLogicCondition(1, new LogicCondition(
+          "Is there at least one vaccine dose administered on or before the current vaccine dose administered date?") {// CHeck
+
+        @Override
+        protected LogicResult evaluateInternal() {
+          for (AntigenAdministeredRecord aar : dataModel.getAntigenAdministeredRecordList()) {
+
+            if (!aar.getDateAdministered().after(caDateAdministered.getFinalValue())) {
+              return YES;
+            }
+          }
+          return NO;
+        }
+      });
+
+      setLogicResults(0, YES, NO, ANY);
+      setLogicResults(1, YES, ANY, NO);
+
+      setLogicOutcome(0, new LogicOutcome() {
+        @Override
+        public void perform() {
+          log("Yes. The vaccine dose administered should be evaluated for a live virus conflict");
+        }
+      });
+
+      setLogicOutcome(1, new LogicOutcome() {
+        @Override
+        public void perform() {
+          log("No. The vaccine dose administered should not be evaluated for a live virus conflict.");
+        }
+      });
+
+      setLogicOutcome(2, new LogicOutcome() {
+
+        @Override
+        public void perform() {
+          log("No. The vaccine dose administered should not be evaluated for a live virus conflict.");
+        }
+      });
+
     }
   }
 
-  
-  
-private class LT421 extends LogicTable{
+  private class LT421 extends LogicTable {
 
-	public LT421() {
-		
-		super(1, 2, "Table 4-21 Could the two vaccine dosesadministrated be in conflict ?");
-		
-		setLogicCondition(0, new LogicCondition("Is the vaccine type of the previous vaccine dose administered the same as one of the supporting data defined live virus conflict previous vaccine types when the current vaccine dose administered type is same as the live virus conflict current vaccine type ?") {	
-			@Override
-			protected LogicResult evaluateInternal() {
-				for (LiveVirusConflict lvc :dataModel.getLiveVirusConflictList() ){
-					if(lvc.getPreviousVaccineType().equals(caPreviousVaccineType)){
-						if(lvc.getCurrentVaccineType().equals(caCurrentVaccineType)){
-							return YES;
-						}	
-					}		
-				}
-				return NO;
-			}	
-		});
-		
-		setLogicResults(0, YES);
-		setLogicResults(1,NO);
-		
-		setLogicOutcome(0, new LogicOutcome() {
-			
-			@Override
-			public void perform() {	
-				log("Yes. The two doses must be checked for a live virus conflict");
-			}
-		});
-		
-		setLogicOutcome(1, new LogicOutcome() {
-			
-			@Override
-			public void perform() {
-				log("No. The two doses need not be checked for a live virus conflict");
-				
-			}
-		});
-		
-	}
-	
-}
-  
-private class LT422 extends LogicTable{
-	
-	public LT422() {
-		super(1, 2,"Table 4-22 Is the current vaccine dose administrated in conflict with previous vaccije dose administrated ?");
-		
-		setLogicCondition(0, new LogicCondition("Is the conflict begin interval date ≤ current date administered<conflict end interval date?") {
-			
-			@Override
-			protected LogicResult evaluateInternal() {
-				for(LiveVirusConflict lvc: dataModel.getLiveVirusConflictList()){
-					if(caDateAdministered.getFinalValue().after( caConflictBeginIntervalDate.getAssumedValue()) && caDateAdministered.getFinalValue().before(caConflictEndIntervalDate.getAssumedValue())){
-						return YES;
-					}
-				}
-				
-				return NO;
-			}
-		});
-		
-		setLogicResults(0, YES);
-		setLogicResults(1, NO);
-		
-		
-		setLogicOutcome(0, new LogicOutcome() {
-			
-			@Override
-			public void perform() {
-				log("Yes. The vaccine dose administered is in conflict with a previous vaccine dose administered");				
-			}
-		});
-		
-		setLogicOutcome(1, new LogicOutcome() {
-			
-			@Override
-			public void perform() {
-				log("No. The vaccine dose administered is not in conflict with a previous vaccine dose administered.");
-			}
-		});
-	}
+    public LT421() {
 
-}
-  
-  
-  
+      super(1, 2, "Table 4-21 Could the two vaccine dosesadministrated be in conflict ?");
+
+      setLogicCondition(0, new LogicCondition(
+          "Is the vaccine type of the previous vaccine dose administered the same as one of the supporting data defined live virus conflict previous vaccine types when the current vaccine dose administered type is same as the live virus conflict current vaccine type ?") {
+        @Override
+        protected LogicResult evaluateInternal() {
+          for (LiveVirusConflict lvc : dataModel.getLiveVirusConflictList()) {
+            if (lvc.getPreviousVaccineType().equals(caPreviousVaccineType)) {
+              if (lvc.getCurrentVaccineType().equals(caCurrentVaccineType)) {
+                return YES;
+              }
+            }
+          }
+          return NO;
+        }
+      });
+
+      setLogicResults(0, YES);
+      setLogicResults(1, NO);
+
+      setLogicOutcome(0, new LogicOutcome() {
+
+        @Override
+        public void perform() {
+          log("Yes. The two doses must be checked for a live virus conflict");
+        }
+      });
+
+      setLogicOutcome(1, new LogicOutcome() {
+
+        @Override
+        public void perform() {
+          log("No. The two doses need not be checked for a live virus conflict");
+
+        }
+      });
+
+    }
+
+  }
+
+  private class LT422 extends LogicTable {
+
+    public LT422() {
+      super(1, 2,
+          "Table 4-22 Is the current vaccine dose administrated in conflict with previous vaccije dose administrated ?");
+
+      setLogicCondition(0, new LogicCondition(
+          "Is the conflict begin interval date ≤ current date administered<conflict end interval date?") {
+
+        @Override
+        protected LogicResult evaluateInternal() {
+          for (LiveVirusConflict lvc : dataModel.getLiveVirusConflictList()) {
+            if (caDateAdministered.getFinalValue().after(caConflictBeginIntervalDate.getAssumedValue())
+                && caDateAdministered.getFinalValue().before(caConflictEndIntervalDate.getAssumedValue())) {
+              return YES;
+            }
+          }
+
+          return NO;
+        }
+      });
+
+      setLogicResults(0, YES);
+      setLogicResults(1, NO);
+
+      setLogicOutcome(0, new LogicOutcome() {
+
+        @Override
+        public void perform() {
+          log("Yes. The vaccine dose administered is in conflict with a previous vaccine dose administered");
+        }
+      });
+
+      setLogicOutcome(1, new LogicOutcome() {
+
+        @Override
+        public void perform() {
+          log("No. The vaccine dose administered is not in conflict with a previous vaccine dose administered.");
+        }
+      });
+    }
+
+  }
 
 }
