2a795ccdc61a0794d6b35cb271162440d30edbf4
Mohamed <Mohamed@DESKTOP-UOGMCTG>
2017-03-15 18:22:14 +0100

    Changes in generates forcast dates

--- src/main/java/org/openimmunizationsoftware/cdsi/core/logic/GenerateForecastDates.java
+++ src/main/java/org/openimmunizationsoftware/cdsi/core/logic/GenerateForecastDates.java
@@ -129,7 +129,7 @@ public class GenerateForecastDates extends LogicStep {
 		  ////System.err.println("earliestRecommendedInterval is null");
 	  }	  
   }
-  //
+  
   
   private void findLatestRecommendedIntervalDate(){
 	  TimePeriod latestRecommendedInterval;
@@ -178,7 +178,7 @@ public class GenerateForecastDates extends LogicStep {
 	    
   }
   
-  int size = dataModel.getForecastList().size();
+  int size = dataModel.getVaccineGroupForecast().getForecastList().size();
 
   public GenerateForecastDates(DataModel dataModel) {
     super(LogicStepType.GENERATE_FORECAST_DATES_AND_RECOMMEND_VACCINES, dataModel);
@@ -231,19 +231,19 @@ public class GenerateForecastDates extends LogicStep {
 	  Forecast forecast = new Forecast();
 	  forecast.setAntigen(dataModel.getPatientSeries().getTrackedAntigenSeries().getTargetDisease());
 	  forecast.setTargetDose(dataModel.getTargetDose());
-	  forecast.setVaccineGroupForecast(null); // todo do we create the vaccine group forecast now? or later? 
+	  forecast.setVaccineGroupForecast(dataModel.getVaccineGroupForecast());
 	  generateForcastDates(forecast);
 	  
 	  Antigen newAntigenForeCast = forecast.getAntigen();
 	  List<Antigen> antigenFromForcastList = new ArrayList<Antigen>();
-	  List<Forecast> forecastList = dataModel.getForecastList();
+	  List<Forecast> forecastList = dataModel.getVaccineGroupForecast().getForecastList();
 	  
     for(Forecast foreCast:forecastList){
     	antigenFromForcastList.add(foreCast.getAntigen());
     }
     
     if(!antigenFromForcastList.contains(newAntigenForeCast)){
-    	 dataModel.getForecastList().add(forecast);
+    	 dataModel.getVaccineGroupForecast().getForecastList().add(forecast);
     }
 
     setNextLogicStepType(LogicStepType.FOR_EACH_PATIENT_SERIES);
@@ -358,8 +358,8 @@ public class GenerateForecastDates extends LogicStep {
 			 if(biggeestAmout!=0 || patientReferenceDoseDate!=null){
 				 unadjustedRecommandedDate = DateUtils.addDays(patientReferenceDoseDate, biggeestAmout);
 			 }else{
-				 if(dataModel.getForecastList().size()>0){
-					 Forecast forecast = dataModel.getForecastList().get(dataModel.getForecastList().size()-1);
+				 if(dataModel.getVaccineGroupForecast().getForecastList().size()>0){
+					 Forecast forecast = dataModel.getVaccineGroupForecast().getForecastList().get(dataModel.getVaccineGroupForecast().getForecastList().size()-1);
 					 unadjustedRecommandedDate = forecast.getEarliestDate();
 				 } 
 			 }	 
