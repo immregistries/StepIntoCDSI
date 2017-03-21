2d78d97e56aa9b63db5980c2475608d92b50cef1
Mohamed <Mohamed@DESKTOP-UOGMCTG>
2017-03-21 16:24:50 +0100

    In Process Patient Series

--- src/main/java/org/openimmunizationsoftware/cdsi/core/logic/InProcessPatientSeries.java
+++ src/main/java/org/openimmunizationsoftware/cdsi/core/logic/InProcessPatientSeries.java
@@ -1,14 +1,116 @@
 package org.openimmunizationsoftware.cdsi.core.logic;
 
 import java.io.PrintWriter;
+import java.util.List;
 
 import org.openimmunizationsoftware.cdsi.core.data.DataModel;
+import org.openimmunizationsoftware.cdsi.core.domain.PatientSeries;
+import org.openimmunizationsoftware.cdsi.core.domain.TargetDose;
+import org.openimmunizationsoftware.cdsi.core.domain.datatypes.TargetDoseStatus;
+import org.openimmunizationsoftware.cdsi.core.domain.datatypes.YesNo;
 import org.openimmunizationsoftware.cdsi.core.logic.items.LogicTable;
 
 public class InProcessPatientSeries extends LogicStep
 {
 
   // private ConditionAttribute<Date> caDateAdministered = null;
+	
+	private List<PatientSeries> patientSeriesList = dataModel.getPatientSeriesList();
+	
+	/***
+	 * cond1
+	 * A candidate patient series is a product patient series and has all valid doses.
+	 * 
+	 * P66 SelectB-23 "patient path" or "product path"
+	 */
+	
+	private int cond1(PatientSeries patientSeries){
+		boolean productPatientSeries = false;
+		boolean hasAllValidDoses = false;
+		
+		
+		if(patientSeries.getTrackedAntigenSeries().getSelectBestPatientSeries().getProductPath()!=null){
+			if(patientSeries.getTrackedAntigenSeries().getSelectBestPatientSeries().getProductPath().equals(YesNo.YES)){
+				productPatientSeries = true;
+			}
+			
+		}
+		
+		for(TargetDose target:patientSeries.getTargetDoseList()){
+			if(target.getTargetDoseStatus()!=null){
+				if(!target.getTargetDoseStatus().equals(TargetDoseStatus.SATISFIED)){
+					
+				}else{
+					hasAllValidDoses = true;
+				}
+			}
+			
+		}
+		
+		if(productPatientSeries && hasAllValidDoses){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	
+	/**
+	 * Cond2
+	 * A candidate patient series is completable.
+	 */
+	
+	private int cond2(PatientSeries patientSeries){
+		return 0;
+	}
+	
+	/**
+	 * cond3
+	 * Number of valid doses
+	 */
+	
+	private int  numberOfValidDoses (PatientSeries patientSeries){
+		int nbOfValidDoses = 0;
+		for(TargetDose target:patientSeries.getTargetDoseList()){
+			if(target.getTargetDoseStatus()!=null){
+				if(target.getTargetDoseStatus().equals(TargetDoseStatus.SATISFIED)){
+					nbOfValidDoses++;
+				}
+			}
+			
+		}
+		return nbOfValidDoses;
+	}
+	
+	/**
+	 * Cond4
+	 * A candidate patient series is closest to completion.
+	 */
+	private int isClosestToCompletion(PatientSeries patientSeries){
+		return 0;
+	}
+	
+	/**
+	 * Cond5
+	 * A candidate patient series can finish earliest.
+	 */
+	
+	private int canFinishEarliest(PatientSeries patientSeries){
+		return 0;
+	}
+	
+	/**
+	 * Cond6
+	 * A candidate patient series exceeded maximum age to start
+	 */
+	
+	private int exceededTheMaximumAgeToStart(PatientSeries patientSeries){
+		int maximumAgetoStart;
+		return 0;
+	}
+	
+	
+
+ 
 
   public InProcessPatientSeries(DataModel dataModel)
   {
