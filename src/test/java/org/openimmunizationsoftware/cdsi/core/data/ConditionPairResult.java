e2080162132dd5bd873ece6f33419f8ab7d98218
Amit <amitf82@gmail.com>
2024-08-15 12:59:47 -0600

    reworked so it now can deal with varaibel number of conditions

--- src/test/java/org/openimmunizationsoftware/cdsi/core/data/ConditionPairResult.java
+++ src/test/java/org/openimmunizationsoftware/cdsi/core/data/ConditionPairResult.java
@@ -10,11 +10,10 @@ import java.util.Iterator;
 import org.openimmunizationsoftware.cdsi.core.logic.items.LogicResult;
 import org.openimmunizationsoftware.cdsi.core.domain.datatypes.DoseCondition;
 
+import java.util.Map;
+import java.util.HashMap;
 
-import java.util.Arrays;
-
-public enum ConditionPairResult implements Iterable<ConditionPairResult.ConditionResultValuePairs<?>> {
-
+public enum ConditionPairResult implements Iterable<ConditionPairResult.ConditionResult> {
     Table6_3 {
         @Override
         protected void initializePairs() {
@@ -22,34 +21,88 @@ public enum ConditionPairResult implements Iterable<ConditionPairResult.Conditio
             Date lotExpGood = new Date(dateAdmin.getTime() - 1000);
             Date lotExpBad = new Date(dateAdmin.getTime() + 1000);
 
-            conditionPairs = Arrays.asList(
-                new ConditionResultValuePairs<Date>(
+            conditionPairs = new HashMap<>();
+            conditionPairs.put("Date", Arrays.asList(
+                new ConditionResultValuePairs<>(
                     Arrays.asList(dateAdmin, dateAdmin, dateAdmin),
                     Arrays.asList(lotExpGood, lotExpBad, dateAdmin),
                     Arrays.asList(LogicResult.NO, LogicResult.YES, LogicResult.NO)
-                ),
-                new ConditionResultValuePairs<DoseCondition>(
-                    Arrays.asList(DoseCondition.YES, DoseCondition.NO),
-                    Arrays.asList(LogicResult.YES, LogicResult.NO)
                 )
-            );
+            ));
+            conditionPairs.put("DoseCondition", Arrays.asList(
+                new ConditionResultValue<>(
+                    Arrays.asList(DoseCondition.YES, DoseCondition.NO),
+                    Arrays.asList(LogicResult.NO, LogicResult.NO) 
+                )
+            ));
         }
     };
 
-    List<ConditionResultValuePairs<?>> conditionPairs;
+    // Updated to use the common interface
+    protected Map<String, List<ConditionResult>> conditionPairs;
     protected abstract void initializePairs();
 
-   
     private ConditionPairResult() {
         initializePairs();
     }
 
     @Override
-    public Iterator<ConditionResultValuePairs<?>> iterator() {
-        return conditionPairs.iterator();
+    public Iterator<ConditionResult> iterator() {
+        return new ConditionPairResultIterator();
     }
 
-    public class ConditionResultValuePairs<T> implements Iterable<List<Object>> {
+    private class ConditionPairResultIterator implements Iterator<ConditionResult> {
+        private final Iterator<List<ConditionResult>> mapIterator;
+        private Iterator<ConditionResult> listIterator;
+
+        public ConditionPairResultIterator() {
+            mapIterator = conditionPairs.values().iterator();
+            if (mapIterator.hasNext()) {
+                listIterator = mapIterator.next().iterator();
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (listIterator != null && listIterator.hasNext()) || (mapIterator.hasNext());
+        }
+
+        @Override
+        public ConditionResult next() {
+            if (listIterator != null && listIterator.hasNext()) {
+                return listIterator.next();
+            } else if (mapIterator.hasNext()) {
+                listIterator = mapIterator.next().iterator();
+                return listIterator.next();
+            }
+            throw new NoSuchElementException();
+        }
+    }
+
+    public interface ConditionResult {
+        abstract List<Object> get(int index);
+        abstract int size();
+    }
+
+    public static class ConditionResultValue<T> implements ConditionResult {
+        private List<T> first;
+        private List<LogicResult> result;
+
+        public ConditionResultValue(List<T> first, List<LogicResult> result) {
+            this.first = first;
+            this.result = result;
+        }
+
+        public List<Object> get(int index) {
+            return Arrays.asList(first.get(index), result.get(index));
+        }
+
+        public int size() {
+            return first.size();
+        }
+    }
+
+    public static class ConditionResultValuePairs<T>  implements ConditionResult {
         private List<T> first;
         private List<T> second;
         private List<LogicResult> result;
@@ -60,28 +113,34 @@ public enum ConditionPairResult implements Iterable<ConditionPairResult.Conditio
             this.result = result;
         }
 
-        @Override
-        public Iterator<List<Object>> iterator() {
-            return new CustomIterator();
+        public List<Object> get(int index) {
+            return Arrays.asList(first.get(index), second.get(index), result.get(index));
         }
 
-        private class CustomIterator implements Iterator<List<Object>> {
-            private int index = 0;
+        public int size() {
+            return first.size();
+        }
+    }
 
-            @Override
-            public boolean hasNext() {
-                return index < first.size() && index < second.size() && index < result.size();
-            }
+    public static class ConditionResultValueTriplet<T> implements ConditionResult {
+        private List<T> first;
+        private List<T> second;
+        private List<T> third;
+        private List<LogicResult> result;
+        
+        public ConditionResultValueTriplet(List<T> first, List<T> second, List<T> third, List<LogicResult> result) {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+            this.result = result;
+        }
 
-            @Override
-            public List<Object> next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                List<Object> nextElement = Arrays.asList(first.get(index), second.get(index), result.get(index));
-                index++;
-                return nextElement;
-            }
+        public List<Object> get(int index) {
+            return Arrays.asList(first.get(index), second.get(index), third.get(index), result.get(index));
+        }
+
+        public int size() {
+            return first.size();
         }
     }
 }
