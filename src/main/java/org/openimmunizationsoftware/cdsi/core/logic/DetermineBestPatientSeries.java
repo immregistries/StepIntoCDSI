14e6dbc4edb34f0f2ffa50ada2b585e39021fad2
jonnyspy <36827944+jonnyspy@users.noreply.github.com>
2025-01-23 09:45:43 -0700

    Added table to 8.8

--- src/main/java/org/openimmunizationsoftware/cdsi/core/logic/DetermineBestPatientSeries.java
+++ src/main/java/org/openimmunizationsoftware/cdsi/core/logic/DetermineBestPatientSeries.java
@@ -8,61 +8,43 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
 
+import org.apache.jena.sparql.function.library.leviathan.log;
 import org.openimmunizationsoftware.cdsi.core.data.DataModel;
+import org.openimmunizationsoftware.cdsi.core.domain.AntigenSeries;
 import org.openimmunizationsoftware.cdsi.core.domain.PatientSeries;
+import org.openimmunizationsoftware.cdsi.core.domain.SeriesType;
+import org.openimmunizationsoftware.cdsi.core.domain.TargetDose;
+import org.openimmunizationsoftware.cdsi.core.domain.VaccineDoseAdministered;
+import org.openimmunizationsoftware.cdsi.core.domain.datatypes.PatientSeriesStatus;
+import org.openimmunizationsoftware.cdsi.core.domain.datatypes.YesNo;
+import org.openimmunizationsoftware.cdsi.core.logic.items.LogicCondition;
+import org.openimmunizationsoftware.cdsi.core.logic.items.LogicOutcome;
+import org.openimmunizationsoftware.cdsi.core.logic.items.LogicResult;
+import org.openimmunizationsoftware.cdsi.core.logic.items.LogicTable;
 
 public class DetermineBestPatientSeries extends LogicStep {
-
     private List<PatientSeries> patientSeriesList = dataModel.getSelectedPatientSeriesList();
 
     public DetermineBestPatientSeries(DataModel dataModel) {
         super(LogicStepType.DETERMINE_BEST_PATIENT_SERIES, dataModel);
-        setConditionTableName("Table ");
 
-    }
-
-    private LinkedHashMap<PatientSeries, Integer> sortByComparator(
-            LinkedHashMap<PatientSeries, Integer> unsortMap) {
-
-        List<Entry<PatientSeries, Integer>> list = new LinkedList<Entry<PatientSeries, Integer>>(unsortMap.entrySet());
-
-        // Sorting the list based on values
-        Collections.sort(list, new Comparator<Entry<PatientSeries, Integer>>() {
-
-            @Override
-            public int compare(Entry<PatientSeries, Integer> o1, Entry<PatientSeries, Integer> o2) {
-                // Sort Desc
-                return o2.getValue().compareTo(o1.getValue());
-            }
-        });
-
-        // Maintaining insertion order with the help of LinkedList
-        LinkedHashMap<PatientSeries, Integer> sortedMap = new LinkedHashMap<PatientSeries, Integer>();
-        for (Entry<PatientSeries, Integer> entry : list) {
-
-            sortedMap.put(entry.getKey(), entry.getValue());
-
+        for(PatientSeries ps : patientSeriesList) {
+            LT logicTable = new LT();
+            logicTable.pps = ps;
+            logicTableList.add(logicTable);
         }
-
-        return sortedMap;
     }
 
     private LinkedHashMap<PatientSeries, Integer> patientSeriesMap = new LinkedHashMap<PatientSeries, Integer>();
     private PatientSeries bestPatientSeries = null;
 
-    private void selectBestPatientSeries() {
-        for (PatientSeries patientSeries : patientSeriesList) {
-            patientSeriesMap.put(patientSeries, patientSeries.getScorePatientSeries());
-        }
-        patientSeriesMap = (LinkedHashMap<PatientSeries, Integer>) sortByComparator(patientSeriesMap);
-        if (patientSeriesMap.size() > 0) {
-            bestPatientSeries = (PatientSeries) patientSeriesMap.keySet().toArray()[0];
-        }
-    }
 
     @Override
     public LogicStep process() throws Exception {
-        selectBestPatientSeries();
+        evaluateLogicTables();
+        if(patientSeriesList != null && patientSeriesList.size() > 0) {
+            bestPatientSeries = patientSeriesList.get(0);
+        }
         if (bestPatientSeries != null) {
             dataModel.getBestPatientSeriesList().add(bestPatientSeries);
         }
@@ -87,11 +69,102 @@ public class DetermineBestPatientSeries extends LogicStep {
     }
 
     private void printStandard(PrintWriter out) {
-        out.println(
-                "<p>Select best candidate patient series  provides the business rules to  be applied to  the scored candidate patient series which will result in the best patient series for the patient.</p>");
-
         out.print("<h4> " + dataModel.getAntigen().getName() + " </h4>");
         printBestPatientSeries(out);
     }
 
+    private class LT extends LogicTable {
+        PatientSeries pps;
+        public LT() {
+            super(5,3, "TABLE 8-14 IS THE PRIORITIZED PATIENT SERIES THE BEST PATIENT SERIES FOR THE SERIES GROUP?");
+
+            setLogicCondition(0, new LogicCondition("Is the prioritized patient series a complete patient series?") {
+                @Override
+                protected LogicResult evaluateInternal() {
+                    if(pps.getPatientSeriesStatus() == null || pps.getPatientSeriesStatus() != PatientSeriesStatus.NOT_COMPLETE) {
+                        return LogicResult.NO;
+                    }
+                    return LogicResult.YES;
+                }
+            });
+
+            setLogicCondition(1, new LogicCondition("Is there a prioritized patient series that is a complete patient series in an equivalent series group?") {
+                @Override
+                protected LogicResult evaluateInternal() {
+                    for(PatientSeries ps : patientSeriesList) {
+                        if(ps.getPatientSeriesStatus() != PatientSeriesStatus.COMPLETE) {
+                            return LogicResult.YES;
+                        }
+                    }
+                    return LogicResult.NO;
+                }
+            });
+
+            setLogicCondition(2, new LogicCondition("Is the series type of the prioritized patient series 'Evaluation Only'?") {
+                @Override
+                protected LogicResult evaluateInternal() {
+                    if(pps.getTrackedAntigenSeries().getSeriesType().equals(SeriesType.EVALUATION_ONLY)) {
+                        return LogicResult.YES;
+                    }
+                    return LogicResult.NO;
+                }
+            });
+
+            setLogicCondition(3, new LogicCondition("Is the series type of the prioritized patient series 'Risk'?") {
+                @Override
+                protected LogicResult evaluateInternal() {
+                    if(pps.getTrackedAntigenSeries().getSeriesType().equals(SeriesType.RISK)) {
+                        return LogicResult.YES;
+                    }
+                    return LogicResult.NO;
+                }
+            });
+
+            setLogicCondition(4, new LogicCondition("Is there a prioritized patient series with a series type of 'Risk' in an equivalent series group?") {
+                @Override
+                protected LogicResult evaluateInternal() {
+                    for(PatientSeries ps : patientSeriesList) {
+                        if(ps.getTrackedAntigenSeries().getSeriesType().equals(SeriesType.RISK)) {
+                            return LogicResult.YES;
+                        }
+                    }
+                    return LogicResult.NO;
+                }
+            });
+
+            setLogicResults(0, LogicResult.YES, LogicResult.NO, LogicResult.NO);
+            setLogicResults(1, LogicResult.ANY, LogicResult.NO, LogicResult.NO);
+            setLogicResults(2, LogicResult.ANY, LogicResult.NO, LogicResult.NO);
+            setLogicResults(3, LogicResult.ANY, LogicResult.YES, LogicResult.NO);
+            setLogicResults(4, LogicResult.ANY, LogicResult.ANY, LogicResult.NO);
+
+            setLogicOutcomeDefault(new LogicOutcome() {
+                @Override
+                public void perform() {
+                    log("No. There is no best patient series for the series group.");
+                }
+            });
+
+            setLogicOutcome(0, new LogicOutcome() {
+                @Override
+                public void perform() {
+                    log("Yes. The prioritized patient series is the best patient series for the series group.");
+                }
+            });
+
+            setLogicOutcome(1, new LogicOutcome() {
+                @Override
+                public void perform() {
+                    log("Yes. The prioritized patient series is the best patient series for the series group.");
+                }
+            });
+
+            setLogicOutcome(2, new LogicOutcome() {
+                @Override
+                public void perform() {
+                    log("Yes. The prioritized patient series is the best patient series for the series group.");
+                }
+            });
+        }
+    }
 }
