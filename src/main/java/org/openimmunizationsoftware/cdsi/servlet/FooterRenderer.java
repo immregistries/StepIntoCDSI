87f018f9295ed6577e1da4c27331246344e63682
Nathan Bunker <nbunker@immregistries.org>
2026-03-05 06:42:14 -0700

    Added terms and policies to footer

--- src/main/java/org/openimmunizationsoftware/cdsi/servlet/FooterRenderer.java
+++ src/main/java/org/openimmunizationsoftware/cdsi/servlet/FooterRenderer.java
@@ -56,7 +56,9 @@ public final class FooterRenderer {
                 + "<p><strong>American Immunization Registry Association (AIRA)</strong> supports collaboration, standards, and shared solutions that strengthen immunization information systems and improve the use of vaccination data to protect public health. "
                 + "<a href=\"https://www.immregistries.org/\">https://www.immregistries.org/</a></p>"
                 + "<p style=\"margin-top:20px;\">Step Into CDSi version " + escapeHtml(SoftwareVersion.VERSION)
-                + " &dash; &copy; Copyright 2026, American Immunization Registry Association. All rights reserved.</p>"
+                + " &dash; &copy; Copyright 2026, American Immunization Registry Association. All rights reserved. "
+                + "<a href=\"https://aira.memberclicks.net/assets/docs/Organizational_Docs/AIRA%20Privacy%20Policy%20-%20Final%202024_.pdf\">Privacy Policy</a> "
+                + "<a href=\"https://repository.immregistries.org/files/AIRA-Terms_of_Use_2024.pdf\">Terms of Use</a></p>"
                 + "</footer>";
     }
 
